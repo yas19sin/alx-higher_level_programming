701c96732a5c7365732624e6e5af32df3ca06bdb
yas19sin <yas19sin@gmail.com>
2024-01-03 06:07:00 +0100

    fixi

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(char)), end="")
-print()
+for alpha in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(alpha)), end='')
