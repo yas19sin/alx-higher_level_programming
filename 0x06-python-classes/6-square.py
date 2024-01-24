60a9da75ade4badc48fb06d4479b605559a7df15
yas19sin <yas19sin@gmail.com>
2024-01-24 05:36:38 +0100

    6 fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Defines a square."""
 
 
 class Square:
-    """A square with size and position."""
+    """Represents a square."""
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a Square with size and position."""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+        """
         self.size = size
         self.position = position
 
@@ -17,8 +22,10 @@ class Square:
     @size.setter
     def size(self, value):
         """Set the size of the square with validation."""
-        if not isinstance(value, int) or value < 0:
-            raise ValueError("size must be a non-negative integer")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
@@ -30,9 +37,9 @@ class Square:
     def position(self, value):
         """Set the position of the square with validation."""
         if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(i, int) and i >= 0 for i in value):
-            raise ValueError("position must be a tuple of \
-            two non-negative integers")
+                not all(isinstance(i, int) for i in value) or \
+                any(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -40,7 +47,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square with respect to position."""
+        """Print the square."""
         if self.__size == 0:
             print()
         else:
