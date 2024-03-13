389a0ca599d0365a273a362fd4ea65deccb63c47
Yassine Ennaour <yas19sin@gmail.com>
2024-03-13 06:04:53 +0000

    Update 11-best_score.sql

--- 0x0D_SQL_introduction/11-best_score.sql
+++ 0x0D_SQL_introduction/11-best_score.sql
@@ -1,6 +1,3 @@
--- lists all rows of the table second_table
--- from the database hbtn_0c_0
--- results display score and name
--- ordered by score
--- with score >= 10
+-- this will list all rows of the table second_table from the database hbtn_0c_0
+-- it results display score and name ordered by score with score >= 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
