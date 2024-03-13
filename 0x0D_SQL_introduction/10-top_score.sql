1e9526538a266ab35da8053b011c77950a74e716
Yassine Ennaour <yas19sin@gmail.com>
2024-03-13 05:58:30 +0000

    Update 10-top_score.sql

--- 0x0D_SQL_introduction/10-top_score.sql
+++ 0x0D_SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
--- lists all rows of the table second_table
--- from the database hbtn_0c_0
--- results display score and name
--- ordered by score
+-- liting all the rows of the table second_table from the database hbtn_0c_0
+-- it will display the score and name ordered by score
 SELECT score, name FROM second_table ORDER BY score DESC;
