afc2556da09266df4c3ace57f07343799e8ae723
Yassine Ennaour <yas19sin@gmail.com>
2024-03-13 06:02:32 +0000

    Update 102-top_city.sql

--- 0x0D_SQL_introduction/102-top_city.sql
+++ 0x0D_SQL_introduction/102-top_city.sql
@@ -1,7 +1,4 @@
--- displays the average temp of 3 cities
--- during July and August
--- ordered by temperature desc
-
+-- this will display the average temp of 3 cities during July and August ordered by temperature desc
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
