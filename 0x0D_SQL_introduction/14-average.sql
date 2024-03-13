b8201e239b35ca5e07bb4f4edb4cc89652114e7c
Yassine Ennaour <yas19sin@gmail.com>
2024-03-13 06:07:01 +0000

    Update 14-average.sql

--- 0x0D_SQL_introduction/14-average.sql
+++ 0x0D_SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
--- computes score average of all records in the second_table
--- second table
+-- This computes the score average of all records in the second_table
 SELECT AVG(score) AS average FROM second_table;
