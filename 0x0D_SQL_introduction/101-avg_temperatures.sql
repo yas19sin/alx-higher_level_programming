d60358b0197ae5e7fe7a29275a7ce32ea7886575
Yassine Ennaour <yas19sin@gmail.com>
2024-03-13 06:01:10 +0000

    Update 101-avg_temperatures.sql

--- 0x0D_SQL_introduction/101-avg_temperatures.sql
+++ 0x0D_SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,4 @@
--- displays teh average temp by city
--- ordered by temperature desc
-
+-- displays the average temp by city ordered by temperature desc
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
