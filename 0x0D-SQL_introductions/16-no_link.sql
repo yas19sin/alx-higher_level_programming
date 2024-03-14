ba0c9719c56d50257bb0af2b4724c6b45faf3dfe
yas19sin <yas19sin@gmail.com>
2024-03-14 03:52:01 +0000

    done

---
+++ 0x0D-SQL_introductions/16-no_link.sql
@@ -0,0 +1,3 @@
+-- A script that lists all records of the table 
+-- second_table of the database hbtn_0c_0 in the MySQL server.
+SELECT score, name  FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
---
+++ 0x0D-SQL_introductions/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- A script that lists all the tables of a database in the MySQL server.
+SHOW TABLES;
