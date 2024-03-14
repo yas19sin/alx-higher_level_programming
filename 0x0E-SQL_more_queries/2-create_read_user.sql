102501e1669dd862e66e344ae66e7fc9b55b3d42
yas19sin <yas19sin@gmail.com>
2024-03-14 05:54:22 +0000

    done

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create the user user_0d_2 with SELECT privilege in hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
