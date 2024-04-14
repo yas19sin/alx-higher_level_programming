6fda3705ce4cd1acb0ed73c4eaf080845a07b8e4
yas19sin <yas19sin@gmail.com>
2024-04-14 20:51:00 +0100

    1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Select all states from the database hbtn_0e_0_usa and print them.
+
+Usage: python3 0-select_states.py <user> <password> <database>
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
