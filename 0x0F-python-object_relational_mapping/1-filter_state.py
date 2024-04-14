868554e96d32ef33a52efeca0185f28150c30be2
yas19sin <yas19sin@gmail.com>
2024-04-14 21:02:19 +0100

    3.

--- 0x0F-python-object_relational_mapping/1-filter_state.py
+++ 0x0F-python-object_relational_mapping/1-filter_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY
                     states.id ASC""")
 
     for row in cursor.fetchall():
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                   (sys.argv[4],))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(sys.argv[4]))
 
     for row in cursor.fetchall():
         print(row)
