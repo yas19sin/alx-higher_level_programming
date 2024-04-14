0c07b31beb61dfe38a291941a46e1d8e9e383f79
yas19sin <yas19sin@gmail.com>
2024-04-14 22:22:31 +0100

    done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 """Script to safely filter the states in the database hbtn_0e_0_usa
  and print them."""
-import sys
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-
-    cursor = db.cursor()
-    state_name = MySQLdb.escape_string(sys.argv[4])
-
-    cursor.execute("""SELECT * FROM states WHERE name = %s
-                    ORDER BY states.id ASC""", (state_name,))
-
-    for row in cursor.fetchall():
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
