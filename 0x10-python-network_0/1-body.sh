5667d96b03228e68afe457677f72c1fc722877ee
yas19sin <yas19sin@gmail.com>
2024-04-26 03:25:54 +0100

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Fetch and display the response body for successful (200) requests to a URL.
 # Usage: ./1-body.sh 0.0.0.0:5000/route_1 ; echo ""
-curl --silent --location --request GET "$1" | cat
+curl -sL "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Submits a JSON payload via a POST request to the given URL.
-curl -s -H "Content-Type: application/json" -d "$1" "$2"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
